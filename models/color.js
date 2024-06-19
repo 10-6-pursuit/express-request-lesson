3e50f4a5ba2314776c50d29a11f76fa9a7a9f17f
RazEfron <razefron01@gmail.com>
2024-06-18 20:35:22 -0400

    update color.js to include ids

--- models/color.js
+++ models/color.js
@@ -1,8 +1,8 @@
 module.exports = [
-  "MistyRose",
-  "Coral",
-  "Goldenrod",
-  "MediumAquamarine",
-  "DeepSkyBlue",
-  "Violet",
+  {id: 1, name: "MistyRose"},
+  {id: 2, name: "Coral"},
+  {id: 3, name: "Goldenrod"},
+  {id: 4, name: "MediumAquamarine"},
+  {id: 5, name: "DeepSkyBlue"},
+  {id: 6, name: "Violet"},
 ];
