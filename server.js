a77c79843c74d23340dc423fb1becf248469ee3c
RazEfron <razefron01@gmail.com>
2024-06-18 20:08:23 -0400

    add server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+// Dependencies
+const app = require("./app");
+
+// Configuration
+require("dotenv").config();
+const PORT = process.env.PORT;
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
