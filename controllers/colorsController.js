30b02313983095b44c2b72343ee02196fc7b93fd
RazEfron <razefron01@gmail.com>
2024-06-23 15:28:06 -0400

    add colorsController index and show routes

---
+++ controllers/colorsController.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const colors = express.Router();
+const colorsArray = require("../models/color.js");
+
+// Index
+colors.get("/", (req, res) => {
+  res.json(colorsArray);
+});
+
+// Show
+colors.get("/:id", (req, res) => {
+  // /colors/cool
+  const { id } = req.params;
+  const color = colorsArray.find((color) => color.id === Number(id));
+  if (color) {
+    res.send(color);
+  } else {
+    res.send("Cannot find any colors with this id: " + id);
+  }
+});
+
+module.exports = colors;
